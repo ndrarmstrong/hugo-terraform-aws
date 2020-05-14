8eb8e10b69fa4d8150287222b7698fd6e19bbb36
Nicholas Armstrong <nicholas@nicholasarmstrong.com>
2020-05-13 21:14:52 -0400

    Clarify wording on certificate validation
    * You don't approve it, you validate it

--- infra/site/acm.tf
+++ infra/site/acm.tf
@@ -1,5 +1,5 @@
 # ACM Certificate
-# NB: This will be created by Terraform, but needs to be approved through the console.
+# NB: This will be created by Terraform, but needs to be manually validated before use
 resource "aws_acm_certificate" "site" {
   provider          = aws.us-east-1
   domain_name       = var.domain_name
